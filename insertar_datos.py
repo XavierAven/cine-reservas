641bafc356031e6c4403e8dc3e4adfd4dfd1c775
Xavier Aventin <xaventin@campus.eug.es>
2025-06-06 18:23:27 +0200

    Añadir usuario admin en script de inserción

--- insertar_datos.py
+++ insertar_datos.py
@@ -1,7 +1,8 @@
 from app import create_app
 from app.database import db
-from app.models import Pelicula, Sesion
+from app.models import Pelicula, Sesion, Usuario
 from datetime import date, time
+from werkzeug.security import generate_password_hash
 
 app = create_app()
 app.app_context().push()
@@ -9,9 +10,20 @@ app.app_context().push()
 # Elimina datos anteriores (opcional en desarrollo)
 Sesion.query.delete()
 Pelicula.query.delete()
+Usuario.query.delete()
 db.session.commit()
 
-# Crear película
+# Crear usuario admin
+admin = Usuario(
+    nombre="Administrador",
+    email="admin@cine.com",
+    password=generate_password_hash("admin123"),
+    tipo="admin"
+)
+
+db.session.add(admin)
+
+# Crear películas
 p1 = Pelicula(
     titulo="Interstellar",
     descripcion="Viaje a través del espacio y el tiempo",
@@ -37,4 +49,4 @@ s3 = Sesion(id_pelicula=p2.id, fecha=date.today(), hora=time(18, 30), sala="Sala
 db.session.add_all([s1, s2, s3])
 db.session.commit()
 
-print("Películas y sesiones insertadas correctamente.")
+print("Películas, sesiones y usuario admin insertados correctamente.")
